2cbe7f6a5043590ba2c4ff636ca9399e8c54025c
Mathieu Lecarme <mlecarme@bearstech.com>
2016-04-27 11:52:43 +0200

    Using flags.

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"github.com/mholt/caddy/middleware"
 	"github.com/mholt/caddy/middleware/fastcgi"
@@ -12,6 +13,18 @@ import (
 	"os"
 )
 
+var listen string
+var target string
+var log_path string
+var root string
+
+func init() {
+	flag.StringVar(&listen, "listen", "/var/run/fcgiproxy.sock", "What Apache or Nginx see")
+	flag.StringVar(&target, "target", "/var/run/php5-fpm.sock", "Where is php-fpm")
+	flag.StringVar(&log_path, "log", "/var/log/fcgiproxy.log", "Log path")
+	flag.StringVar(&root, "root", "/var/www", "Where the PHP files live")
+}
+
 type Proxy struct {
 	server middleware.Handler
 }
@@ -22,17 +35,15 @@ func (p Proxy) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
-	root := os.Args[1]
-	source := os.Args[2]
-	dest := os.Args[3]
+	flag.Parse()
 
-	_ = os.Remove(source)
+	_ = os.Remove(listen)
 
-	listener, err := net.Listen("unix", source)
+	listener, err := net.Listen("unix", listen)
 	if err != nil {
 		panic(err)
 	}
-	err = os.Chmod(source, 0770)
+	err = os.Chmod(listen, 0770)
 	if err != nil {
 		panic(err)
 	}
@@ -44,15 +55,14 @@ func main() {
 		AbsRoot: root,
 		Rules: []fastcgi.Rule{{
 			Path:    "/",
-			Address: dest,
+			Address: target,
 			Ext:     "php",
 		},
 		},
 	}
 
-	logPath := "/tmp/access.log"
 	var file *os.File
-	file, err = os.OpenFile(logPath, os.O_RDWR|os.O_CREATE|os.O_APPEND, 0644)
+	file, err = os.OpenFile(log_path, os.O_RDWR|os.O_CREATE|os.O_APPEND, 0644)
 	if err != nil {
 		panic(err)
 	}
@@ -61,7 +71,7 @@ func main() {
 		Next: handler,
 		Rules: []logg.Rule{{
 			PathScope:  "/",
-			OutputFile: logPath,
+			OutputFile: log_path,
 			Format:     "{when} {host} {method} {path} {latency} {size} {status}",
 			Log:        log.New(file, "", 0),
 		},
